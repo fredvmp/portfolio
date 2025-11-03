2fa11e03eee5bcad8a638eb26e7ee61f211751bc
Fred <124618794+fredvmp@users.noreply.github.com>
2025-11-03 01:54:00 +0100

    imágenes vercel

--- frontend/portfolio/src/components/Projects/Projects.tsx
+++ frontend/portfolio/src/components/Projects/Projects.tsx
@@ -1,6 +1,12 @@
 import styles from "./Projects.module.css";
 import ProjectCard from "./ProjectCard";
-
+import chess01 from "../../assets/images/ancient_chess_01.png";
+import chess02 from "../../assets/images/ancient_chess_02.png";
+import chess03 from "../../assets/images/ancient_chess_03.png";
+import library01 from "../../assets/images/hallowed_library_01.png";
+import library02 from "../../assets/images/hallowed_library_02.png";
+import library03 from "../../assets/images/hallowed_library_03.png";
+import library04 from "../../assets/images/hallowed_library_04.png";
 
 function Projects() {
   const projects = [
@@ -9,7 +15,7 @@ function Projects() {
       description:
         "Aplicación web Full Stack para la gestión y descubrimiento de libros, inspirada en Goodreads. Permitirá a los usuarios registrarse, valorar y reseñar lecturas, crear listas personalizadas y recibir recomendaciones. Implementa autenticación JWT y arquitectura REST.",
       type: "Full Stack",
-      images: ["../../src/assets/hallowed_library_01.png", "../../src/assets/hallowed_library_02.png", "../../src/assets/hallowed_library_03.png", "../../src/assets/hallowed_library_04.png"],
+      images: [library01, library02, library03, library04],
       techs: ["react", "typescript", "css3", "spring", "java", "postgresql"],
       github: "https://github.com/fredvmp/hallowed-library",
       demo: "",
@@ -19,7 +25,7 @@ function Projects() {
       description:
         "Aplicación web interactiva que implementa renderizado dinámico del tablero y paneles laterales, control de turnos, detección de jaque mate, registro de piezas eliminadas y un historial de movimientos.",
       type: "Frontend",
-      images: ["../../src/assets/ancient_chess_01.png", "../../src/assets/ancient_chess_02.png", "../../src/assets/ancient_chess_03.png"],
+      images: [chess01, chess02, chess03],
       techs: ["react", "typescript", "css3"],
       github: "https://github.com/fredvmp/ancient_chess",
       demo: "",
